033a510871e98de0b03820df7e26a0420f5722da
Derek Wan <d.wan@icloud.com>
2020-11-29 17:57:53 +0800

    Initial commit (#1)

---
+++ rename_paw_patrol.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import re
+from pathlib import Path
+from re import IGNORECASE
+from re import search
+from string import capwords
+from typing import Iterator
+
+
+__version__ = "0.0.1"
+
+
+def yield_files() -> Iterator[Path]:
+    for p in Path("/data/derek/Paw Patrol/").glob("**/*"):
+        if p.is_file() and not search("Torrent", p.name):
+            yield p
+
+
+PATTERN_RENAME = re.compile(
+    r"(?:Paw\.Patrol)?\.?s(\d{2})e(\d{2})\.([\w\.!'-]+?)(?:\.eng)?(?:\.?\d+p(?:(?:\.[A-Z\d\.-]+)?))\.(mp4|mkv)",  # noqa: E501
+    flags=IGNORECASE,
+)
+
+
+def clean_name(name: str) -> str:
+    if (match := PATTERN_RENAME.search(name)) is not None:
+        season, ep_no, ep_name, ext = match.groups()
+        ep_name = capwords(ep_name.replace(".", " "))
+        return f"S{season}E{ep_no} - {ep_name}.{ext}"
+    else:
+        raise ValueError(f"Could not match the following:\n    {name}")
+
+
+PATTERN_CLEAN = re.compile(r"S\d{2}E\d{2} - [\s\w!'-]+\.[mp4|mkv]")
+
+
+def process_file(path: Path) -> None:
+    old_name = path.name
+    if PATTERN_CLEAN.search(old_name):
+        return
+    else:
+        print(f"Needs processing:\n    {old_name}")
+        new_name = clean_name(old_name)
+        while True:
+            ans = input(  # noqa: S322
+                f"Rename as follows? (y/n)\n    {new_name}\n>>> ",
+            )
+            if ans == "y":
+                new_path = path.parent.joinpath(new_name)
+                path.rename(new_path)
+                return
+            elif ans == "n":
+                break
+
+
+if __name__ == "__main__":
+    for p in yield_files():
+        process_file(p)
---
+++ tests/test_rename_paw_patrol.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from typing import Callable
+from typing import cast
+from typing import TypeVar
+
+from pytest import mark
+
+from rename_paw_patrol import clean_name
+from rename_paw_patrol import PATTERN_CLEAN
+
+T = TypeVar("T")
+
+
+@cast(
+    Callable[[T], T],
+    mark.parametrize(
+        "old, new",
+        [
+            (
+                "PAW.Patrol.S01E23.Pups.and.the.Ghost.Pirate.720p.WEBRip.x264.AAC.mp4",
+                "S01E23 - Pups And The Ghost Pirate.mp4",
+            ),
+            (
+                "Paw.patrol.S03E12.pups.save.the.paw.patroller.eng.1080p.mkv",
+                "S03E12 - Pups Save The Paw Patroller.mkv",
+            ),
+            (
+                "PAW.Patrol.S03E25.Tracker.Joins.the.Pups!.1080p.WEBRip.x264.mkv",
+                "S03E25 - Tracker Joins The Pups!.mkv",
+            ),
+            (
+                "Paw.Patrol.S04E01.Pups.Save.A.Blimp.1080p.WEB-DL.AAC2.0.H.264.mkv",
+                "S04E01 - Pups Save A Blimp.mkv",
+            ),
+            (
+                "Paw.Patrol.S04E13.Pups.Save.The.Carnival.1080p.WEB-DL.AAC.2.0.H264.mkv",  # noqa: E501
+                "S04E13 - Pups Save The Carnival.mkv",
+            ),
+            (
+                "PAW.Patrol.S04E16.Mission.PAW.Pups.Save.The.Royal.Throne.1080p.NICK.WEBRip.AAC2.0.x264-RTN.mkv",  # noqa: E501
+                "S04E16 - Mission Paw Pups Save The Royal Throne.mkv",
+            ),
+            (
+                "PAW.Patrol.S04E32.Sea.Patrol.Pirate.Pups.to.the.Rescue.1080p.NICK.WEB-DL.AAC2.0.x264-RTN.mkv",  # noqa: E501
+                "S04E32 - Sea Patrol Pirate Pups To The Rescue.mkv",
+            ),
+            (
+                "PAW.Patrol.S04E37.Pups.Save.the.Runaway.Turtles.1080p.mkv",
+                "S04E37 - Pups Save The Runaway Turtles.mkv",
+            ),
+        ],
+    ),
+)
+def test_clean_name(old: str, new: str) -> None:
+    assert clean_name(old) == new
+    assert PATTERN_CLEAN.search(new)
