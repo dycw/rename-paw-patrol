b2419dc48a98d1926fe79cc6069938f7603b5be3
Derek Wan <d.wan@icloud.com>
2020-11-29 17:00:19 +0800

    Better testing, 2 cases now

--- rename_paw_patrol.py
+++ rename_paw_patrol.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
+import re
 from pathlib import Path
 from re import IGNORECASE
 from re import search
@@ -17,14 +18,14 @@ def yield_files() -> Iterator[Path]:
             yield p
 
 
+PATTERN_RENAME = re.compile(
+    r"(?:Paw\.Patrol)?\.?s(\d{2})e(\d{2})\.([\w\.\'-]+?)(?:\.?\d+p\.WEB\-DL\.AAC\.?2\.0\.H\.?264)\.?(mp4|mkv)",  # noqa: E501
+    flags=IGNORECASE,
+)
+
+
 def clean_name(name: str) -> str:
-    if (
-        match := search(
-            r"(?:Paw\.Patrol)?\.?s(\d{2})e(\d{2})\.([\w\.\'-]+?)(?:\.?\d+p\.WEB\-DL\.AAC2\.0\.H\.264)\.?(mp4|mkv)",  # noqa: E501
-            name,
-            flags=IGNORECASE,
-        )
-    ) is not None:
+    if (match := PATTERN_RENAME.search(name)) is not None:
         season, ep_no, ep_name, ext = match.groups()
         ep_name = capwords(ep_name.replace(".", " "))
         return f"S{season}E{ep_no} - {ep_name}.{ext}"
@@ -32,16 +33,19 @@ def clean_name(name: str) -> str:
         raise ValueError(f"Could not match the following:\n    {name}")
 
 
+PATTERN_CLEAN = re.compile(r"S\d{2}E\d{2} - [\s\w\'\-]+\.[mp4|mkv]")
+
+
 def process_file(path: Path) -> None:
     old_name = path.name
-    if search(r"S\d{2}E\d{2} - [\s\w\'\-]+\.[mp4|mkv]", old_name):
+    if PATTERN_CLEAN.search(old_name):
         return
     else:
         print(f"Needs processing:\n    {old_name}")
         new_name = clean_name(old_name)
         while True:
             ans = input(  # noqa: S322
-                f"Rename as follows?\n    {new_name}\n'y', 'n'",
+                f"Rename as follows? (y/n)\n    {new_name}\n>>> ",
             )
             if ans == "y":
                 new_path = path.parent.joinpath(new_name)
--- tests/test_rename_paw_patrol.py
+++ tests/test_rename_paw_patrol.py
@@ -7,6 +7,7 @@ from typing import TypeVar
 from pytest import mark
 
 from rename_paw_patrol import clean_name
+from rename_paw_patrol import PATTERN_CLEAN
 
 T = TypeVar("T")
 
@@ -20,8 +21,13 @@ T = TypeVar("T")
                 "Paw.Patrol.S04E01.Pups.Save.A.Blimp.1080p.WEB-DL.AAC2.0.H.264.mkv",
                 "S04E01 - Pups Save A Blimp.mkv",
             ),
+            (
+                "Paw.Patrol.S04E13.Pups.Save.The.Carnival.1080p.WEB-DL.AAC.2.0.H264.mkv",  # noqa: E501
+                "S04E13 - Pups Save The Carnival.mkv",
+            ),
         ],
     ),
 )
 def test_clean_name(old: str, new: str) -> None:
     assert clean_name(old) == new
+    assert PATTERN_CLEAN.search(new)
